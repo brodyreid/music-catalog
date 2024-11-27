df537b92db483e22d59efff978aae3f160fbdff7
Brody Reid <brodyhreid@gmail.com>
2024-11-26 22:10:30 -0500

    rename

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div className='flex justify-between'>
       <div className="flex items-center gap-1">
         <img src="/logo.svg" alt="logo" className="w-12 h-12 m-4" />
-        <h2 className="text-[#9D74B3]">music_catalog</h2>
+        <h2 className="text-[#9D74B3]">music-catalog</h2>
       </div>
       <div className='flex gap-4 p-4'>
         <Link to={'/projects'} className='hover:brightness-75 duration-100'>projects</Link>
