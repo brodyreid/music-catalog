a56135d2c195a0d9c44b1b5e2ba06fc8e28e2ae4
Brody Reid <brodyhreid@gmail.com>
2025-02-02 02:33:24 -0500

    basic fetches on each page

--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -1,3 +1,75 @@
+import Button from '@/components/ui/Button.tsx';
+import { albumReducer, initialState } from '@/reducers/albumReducer.ts';
+import supabase from '@/supabase.ts';
+import { formatReadableDate } from '@/utils.ts';
+import { useEffect, useReducer, useState } from 'react';
+
 export default function Albums() {
-  return <div>ello</div>;
+  const [isCreating, setIsCreating] = useState(false);
+  const [state, dispatch] = useReducer(albumReducer, initialState);
+  const { all: data, current } = state;
+
+  useEffect(() => {
+    const getAlbums = async () => {
+      const { data, error } = await supabase.from('albums').select(`
+        *,
+        projects ( * )
+        `);
+      if (error) {
+        throw error;
+      }
+
+      dispatch({ type: 'set_all', all: data });
+    };
+
+    getAlbums();
+  }, []);
+
+  return (
+    <>
+      {!(isCreating || state.current) && (
+        <Button
+          onClick={() => {
+            dispatch({ type: 'set_current', current: null });
+            setIsCreating(true);
+          }}>
+          create new album
+        </Button>
+      )}
+      <div className='flex gap-16 mt-16'>
+        <div>
+          <table className='font-mono font-extralight text-sm border-separate border-spacing-2'>
+            <thead>
+              <tr className='text-left border-b'>
+                <th className='pr-3'>title</th>
+                <th className='pr-3'>notes</th>
+                <th className='pr-3'>release_date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((album) => {
+                const { id, title, notes, release_date } = album;
+
+                return (
+                  <tr
+                    key={id}
+                    className={`relative ${!isCreating && 'cursor-pointer hover'} ${current?.id === id && 'font-bold text-orange-300'}`}
+                    onClick={() => {
+                      if (isCreating) {
+                        return;
+                      }
+                      dispatch({ type: 'set_current', current: album });
+                    }}>
+                    <td className='text-nowrap pr-3'>{title}</td>
+                    <td className='text-nowrap pr-3'>{notes}</td>
+                    <td className='text-nowrap pr-3'>{release_date && formatReadableDate(release_date)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Contributors.tsx
+++ src/pages/Contributors.tsx
@@ -1,3 +1,54 @@
+import { contributorReducer, initialState } from '@/reducers/contributorReducer.ts';
+import supabase from '@/supabase.ts';
+import { useEffect, useReducer } from 'react';
+
 export default function Contributors() {
-  return <div>ello</div>;
+  const [state, dispatch] = useReducer(contributorReducer, initialState);
+  const { all: data, current } = state;
+
+  useEffect(() => {
+    const getContributors = async () => {
+      const { data, error } = await supabase.from('contributors').select(`
+        *
+        `);
+      if (error) {
+        throw error;
+      }
+
+      dispatch({ type: 'set_all', all: data });
+    };
+
+    getContributors();
+  }, []);
+
+  return (
+    <>
+      <div className={`flex gap-16 mt-16`}>
+        <table className='font-mono font-extralight text-sm border-separate border-spacing-2'>
+          <thead>
+            <tr className='text-left border-b'>
+              <th className='pr-3'>first_name</th>
+              <th className='pr-3'>artist_name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((contributor) => {
+              const { id, first_name, artist_name } = contributor;
+              return (
+                <tr
+                  key={id}
+                  className={`relative cursor-pointer hover ${current?.id === id && 'font-bold text-orange-300'}`}
+                  onClick={() => {
+                    dispatch({ type: 'set_current', current: contributor });
+                  }}>
+                  <td className='text-nowrap pr-3'>{first_name}</td>
+                  <td className='text-nowrap pr-3'>{artist_name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
